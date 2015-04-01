3e4c1f90a4266db0609382ed7fbdfe8d750d671e
chasetheheyday <chasehd@vt.edu>
2015-04-01 17:02:44 -0400

    updated to include new classes

--- js/custom.js
+++ js/custom.js
@@ -6,10 +6,34 @@ $(document).ready(function() {
     $('#main, #breadcrumb').toggleClass('transparent');
   });
   $('html').click( function(e) {
-	    if ($(e.target).closest('.mobile').length === 0 && $('.row-offcanvas').hasClass('active') && !$(e.target).closest('div').hasClass('sidebar-nav')) {
+	    if ($(e.target).closest('.visible-xs').length === 0 && $('.row-offcanvas').hasClass('active') && !$(e.target).closest('div').hasClass('sidebar-nav')) {
 	    	$('.row-offcanvas').toggleClass('active');
 			$('.showhide').toggle();
 			$('#main, #breadcrumb').toggleClass('transparent');
 	    }
 	});
 });
+
+/* JavaScript Media Queries */
+// $(document).ready(function() {
+// 	if (matchMedia) {
+// 		var mq = window.matchMedia("(max-width: 767px)");
+// 		mq.addListener(WidthChange);
+// 		WidthChange(mq);
+// 	}
+
+
+	// media query change
+// 	function WidthChange(mq) {
+// 		if (mq.matches) {
+// 			$('#copySubmissionEmail').removeClass('input-group-addon');
+// 		}
+// 		else {
+// 			if (!$('#copySubmissionEmail').hasClass('input-group-addon')) {
+// 				$('#copySubmissionEmail').addClass('input-group-addon');
+// 			}
+// 		}
+// 
+// 	}
+// });
+
